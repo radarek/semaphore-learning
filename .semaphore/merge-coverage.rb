6e0563241c8cb1177b505d86b401f207993ecfc3
Radosław Bułat <radek.bulat@gmail.com>
2021-03-27 23:01:03 +0100

    Add inlining html formatter

--- .semaphore/merge-coverage.rb
+++
@@ -1,8 +0,0 @@
-require 'simplecov'
-
-SimpleCov.collate Dir["simplecov-resultset/*/.resultset.json"] do
-  formatter SimpleCov::Formatter::MultiFormatter.new([
-    SimpleCov::Formatter::SimpleFormatter,
-    SimpleCov::Formatter::HTMLFormatter
-  ])
-end
---
+++ .semaphore/merge_coverage.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'base64'
+require 'simplecov'
+
+# Custom formatter that inlines assets into html result.
+# It could be removed if [PR] is merged into simplecov-html gem.
+# [PR]: https://github.com/simplecov-ruby/simplecov-html/pull/107/files
+class InliningFormatter < SimpleCov::Formatter::HTMLFormatter
+  def format(result)
+    File.open(File.join(output_path, 'index.html'), 'wb') do |file|
+      file.puts template('layout').result(binding)
+    end
+    puts output_message(result)
+  end
+
+  private
+
+  def assets_path(name)
+    path = File.join(Gem.loaded_specs['simplecov-html'].full_gem_path, 'public', name)
+
+    # Only have a few content types, just hardcode them
+    content_type = {
+      '.js' => 'text/javascript',
+      '.png' => 'image/png',
+      '.gif' => 'image/gif',
+      '.css' => 'text/css'
+    }.fetch(File.extname(name))
+
+    base64_content = Base64.strict_encode64(File.read(path))
+    "data:#{content_type};base64,#{base64_content}"
+  rescue Errno::ENOENT => e
+    e.message =~ /favicon.png/ ? warn('Warning: favicon.png file is missing') : raise
+  end
+end
+
+SimpleCov.collate Dir['simplecov-resultset/*/.resultset.json'] do
+  formatter InliningFormatter
+end
